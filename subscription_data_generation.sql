ae657ee981872b874c8b3194a4b86ff6b3c40fd9
the-data-driven-co <98104600+the-data-driven-co@users.noreply.github.com>
2022-02-04 14:39:31 -0800

    added skus to conversion data

--- subscription_data_generation.sql
+++ subscription_data_generation.sql
@@ -2,9 +2,9 @@
     select
         row_number() over (order by seq4())::number as customer_id,
         case zipf(5,3,random())
-            when 1 then 'active'
-            when 2 then 'locked'
-            when 3 then 'disabled'
+            when 1 then 'Active'
+            when 2 then 'Locked'
+            when 3 then 'Disabled'
         end as customer_status,
         case zipf(1.5,3,random())
             when 1 then 'Snowflake'
@@ -13,7 +13,7 @@
         end as brand,
         randstr(4,zipf(.5,40,random())) as traffic_source,
         case zipf(1, 5, random())
-            when 1 then 'WorldPay'
+            when 1 then 'Credit Card'
             when 2 then 'Paypal'
             when 3 then 'Apple iTunes'
             when 4 then 'Google Play'
@@ -21,6 +21,14 @@
         end as conversion_payment_processor,
         dateadd(day,-zipf(.03,1096,random()),'2021-12-31') as customer_created,
         customer_created::date as conversion_date,
+        case zipf(2,5,random())
+            when 1 then 'full_suite_subscription'
+            when 2 then 'cloud_only_subscription'
+            when 3 then 'desktop_only_subscription'
+            when 4 then 'desktop_license_annual_updates'
+            when 5 then 'desktop_license_lifetime_updates'
+        end as conversion_sku,
+
 
 
 
